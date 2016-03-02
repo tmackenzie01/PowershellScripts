0464489115302c0658f3381df3722b47073a1863
tmackenzie01 <thomas.mackenzie@vmsuk.com>
2016-03-02 15:12:20 +0000

    dll copy script can now svn revert all previous copied dlls

--- scripts/dllCopy.ps1
+++ scripts/dllCopy.ps1
@@ -9,20 +9,45 @@ $sourceDlls = "$codeRootFolder\VMSColdstore_clean\source\VMSColdstore\bin\Debug\
 			
 $repos = "TitanRecorder_trunk", "TitanRecorder_clean", "TitanVision_clean"
 
-Write-Host "About to copy"$sourceDlls.length"items"
-ForEach ($sourceDll in $sourceDlls)
+# Check arguments for -Revert or -revert
+$revert = $false
+ForEach ($arg in $args)
 {
-	Write-Host "Copying $sourceDll ..."
-	ForEach ($repo in $repos)
-	{
-		$dllFolder = "$codeRootFolder\${repo}\Dependencies_svn\dlls\internal"
-		Write-Host "   to $repo ..."
-		Copy-Item $sourceDll $dllFolder
-		$sourcePdb = $sourceDll -replace ".dll",".pdb"
-		Copy-Item $sourcePdb $dllFolder
-	}
-	Copy-Item $sourceDll "$codeRootFolder"
+  if (($arg -eq '-Revert') -and ($arg -eq '-revert')) {
+    $revert = $true
+  }
 }
+
+if (!$revert) {
+  Write-Host "About to copy"$sourceDlls.length"items"
+  ForEach ($sourceDll in $sourceDlls) {
+	Write-Host "Copying $sourceDll ..."
+	ForEach ($repo in $repos) {
+		  $dllFolder = "$codeRootFolder\${repo}\Dependencies_svn\dlls\internal"
+		  Write-Host "   to $repo ..."
+		  Copy-Item $sourceDll $dllFolder
+		  $sourcePdb = $sourceDll -replace ".dll",".pdb"
+		  Copy-Item $sourcePdb $dllFolder
+	  }
+	  Copy-Item $sourceDll "$codeRootFolder"
+  }
+}
+else {
+  Write-Host "Reverting files"
+  ForEach ($sourceDll in $sourceDlls) {
+	ForEach ($repo in $repos) {
+	  $filenameOnly = [system.io.path]::GetFilename($sourceDll)
+	  $filenameDllOnly = $filenameOnly -replace ".dll",".pdb"
+	  $fullPathDll = "$codeRootFolder\${repo}\Dependencies_svn\dlls\internal\$filenameOnly"
+	  $fullPathPdb = "$codeRootFolder\${repo}\Dependencies_svn\dlls\internal\$filenameDllOnly"
+	  Write-Host "Reverting ... $fullPathDll"
+	  svn revert $fullPathDll
+	  Write-Host "Reverting ... $fullPathPdb"
+	  svn revert $fullPathPdb
+	}
+  }
+}
+
 
 # SIG # Begin signature block
 # MIIFuQYJKoZIhvcNAQcCoIIFqjCCBaYCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
