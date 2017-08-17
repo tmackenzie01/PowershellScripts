2d70efd0dfe5041d743221c4c21fd76bb5427b07
toadordragon <heymrspaceman@users.noreply.github.com>
2017-08-17 21:50:05 +0100

    Added open modules folder script

---
+++ scripts/openModulesFolder.ps1
@@ -0,0 +1,12 @@
+function Get-UserModulePath {
+ 
+    $Path = $env:PSModulePath -split ";" -match $env:USERNAME
+ 
+    if (-not (Test-Path -Path $Path))
+    {
+        New-Item -Path $Path -ItemType Container | Out-Null
+    }
+        $Path
+}
+ 
+Invoke-Item (Get-UserModulePath)
