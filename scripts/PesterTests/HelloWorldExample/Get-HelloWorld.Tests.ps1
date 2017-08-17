c73cd1b61abfd35e76fb4f83ab22b3e097261cd2
toadordragon <heymrspaceman@users.noreply.github.com>
2017-08-17 22:48:00 +0100

    Getting started with Pester

---
+++ scripts/PesterTests/HelloWorldExample/Get-HelloWorld.Tests.ps1
@@ -0,0 +1,11 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+#Actual script is a couple of levels up
+$sut = "..\..\$sut"
+. "$here\$sut"
+
+Describe "Get-HelloWorld" {
+    It "outputs 'Hello world!'" {
+        Get-HelloWorld | Should Be 'Hello world!'
+    }
+}
---
+++ scripts/PesterTests/InstallSetupsTest/InstallSetups.Tests.ps1
@@ -0,0 +1,13 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+#Actual script is a couple of levels up
+$sut = "..\..\$sut"
+. "$here\$sut"
+
+Write-Host $here
+Write-Host $sut
+Describe "InstallSetups" {
+    It "outputs 'Loading data...'" {
+        .\..\..\InstallSetups | Should Be 'No versions found'
+    }
+}
