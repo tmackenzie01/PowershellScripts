bb44fcb74612a244334c58b193ab80c9b0976c6f
tmackenzie01 <thomas.mackenzie@vmsuk.com>
2017-10-26 11:24:13 +0100

    Parse solution script can now output each build config to separate file

--- scripts/parseSolution.ps1
+++ scripts/parseSolution.ps1
@@ -1,4 +1,4 @@
-Param($solutionFile)
+Param($solutionFile, [switch]$outputToFile)
 
 # Load data
 "Loading data..."
@@ -22,21 +22,23 @@ if (!($PSBoundParameters.ContainsKey('solutionFile'))) {
 $output = ""
 $configurations = $($localInfo.VSBuildFlavours)
 $platforms = "x86", "x64"
+$buildConfigCount = 1
 foreach ($configuration in $configurations) {
   foreach ($platform in $platforms) {
+	$buildConfigOutput = ""
     $solutionFileContents = Get-ChildItem $solutionFile
     $lines = $solutionFileContents | Select-String "\.$configuration\|$platform" | Select-Object -Prop Line
     foreach ($line in $lines) {
       $displayLine = $line.Line.Trim()
       $displayLine = $displayLine.Replace(".ActiveCfg","")
       $displayLine = $displayLine.Replace(".Build.0","")
-	
+
       # Get the GUID
       $split = $displayLine.split(".")
       $guid = $split[0]
-	  
+
       $guidSearch = ", ""$guid"""
-	
+
       # Find the project name for the guid
       $projectLine = $solutionFileContents | Select-String "$guidSearch" | Select-Object -Prop Line | select -first 1
       $projectLineSplit = $projectLine.Line.split(",")
@@ -45,15 +47,30 @@ foreach ($configuration in $configurations) {
       $projectGuidNameSplit = $projectGuidName.split("=")
       $projectName = $projectGuidNameSplit[1].trim()
       $projectName = $projectName.replace("""", "")
-      #$output = $output + "##`n"
-      #$output = $output + "FOUND: $projectGuidName`n"
-      #$output = $output + "FOUND: $projectName`n"
-      #$output = $output + "##`n"
+      #$buildConfigOutput = $buildConfigOutput + "##`n"
+      #$buildConfigOutput = $buildConfigOutput + "FOUND: $projectGuidName`n"
+      #$buildConfigOutput = $buildConfigOutput + "FOUND: $projectName`n"
+      #$buildConfigOutput = $buildConfigOutput + "##`n"
 	
-      #$output = $output + "$guid ($projectName)`n"
+      #$buildConfigOutput = $buildConfigOutput + "$guid ($projectName)`n"
       $displayLine = $displayLine.Replace($guid, $projectName)
-      $output = $output + "$displayLine`n"
+      #$buildConfigOutput = $buildConfigOutput + "$displayLine`n"
+	  
+	  $finalLineSplit = $displayLine.split("=")
+	  $projectReleaseConfig = $finalLineSplit[0].PadRight(70)
+	  $solutionReleaseConfig = $finalLineSplit[1]
+      $buildConfigOutput = $buildConfigOutput + "$projectReleaseConfig = $solutionReleaseConfig`n"
     }
+
+	$output = $output + $buildConfigOutput
+
+    if ($PSBoundParameters.ContainsKey('outputToFile')) {
+	  $buildConfigFilename = "D:\$buildConfigCount.txt"
+	  $output = $output + "Writing to file $buildConfigFilename`n"
+	  Set-Content $buildConfigFilename $buildConfigOutput
+	}
+
+	$buildConfigCount = $buildConfigCount + 1
     $output = $output + "`n"
   }  
 }
